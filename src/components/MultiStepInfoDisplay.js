df72a1929802a6fd736485baef750486d4a0576d
Hakimsabkhi <hakimsabkhi001@gmail.com>
2024-04-24 16:12:18 +0200

    4/24/2024

--- src/components/MultiStepInfoDisplay.js
+++ src/components/MultiStepInfoDisplay.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import './MultiStepInfoDisplay.css';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import './MultiStepInfoDisplay.css'
 
 const MultiStepInfoDisplay = () => {
   const prices = {
@@ -8,24 +8,24 @@ const MultiStepInfoDisplay = () => {
     dimensions: 0.0, // Example base price for dimensions
     installationType: 0.0,
     selectedColors: 0.0, // Flat rate for any color selection
-    ManoeuvreType: 0.0, // Example price for ManoeuvreType
-  };
+    ManoeuvreType: 0.0 // Example price for ManoeuvreType
+  }
 
-  const selectedCoulisseColor = useSelector((state) => state.volet.selectedColor.coulisse);
-  const selectedTablierColor = useSelector((state) => state.volet.selectedColor.tablier);
-  const selectedLameFinaleColor = useSelector((state) => state.volet.selectedColor.lameFinale);
-  const lameSelection = useSelector((state) => state.volet.lameSelection);
-  const dimensions = useSelector((state) => state.volet.dimensions);
-  const installationType = useSelector((state) => state.volet.installationType);
-  const ManoeuvreType = useSelector((state) => state.volet.ManoeuvreType);
-  const ManualType = useSelector((state) => state.volet.ManualType);
-  const MotoriseType = useSelector((state) => state.volet.MotoriseType);
-  const TelecommandeType = useSelector((state) => state.volet.TelecommandeType);
-  const InterrupteurType = useSelector((state) => state.volet.InterrupteurType);
-  const SortieDeCableType = useSelector((state) => state.volet.SortieDeCableType);
+  const selectedCoulisseColor = useSelector((state) => state.volet.selectedColor.coulisse)
+  const selectedTablierColor = useSelector((state) => state.volet.selectedColor.tablier)
+  const selectedLameFinaleColor = useSelector((state) => state.volet.selectedColor.lameFinale)
+  const lameSelection = useSelector((state) => state.volet.lameSelection)
+  const dimensions = useSelector((state) => state.volet.dimensions)
+  const installationType = useSelector((state) => state.volet.installationType)
+  const ManoeuvreType = useSelector((state) => state.volet.ManoeuvreType)
+  const ManualType = useSelector((state) => state.volet.ManualType)
+  const MotoriseType = useSelector((state) => state.volet.MotoriseType)
+  const TelecommandeType = useSelector((state) => state.volet.TelecommandeType)
+  const InterrupteurType = useSelector((state) => state.volet.InterrupteurType)
+  const SortieDeCableType = useSelector((state) => state.volet.SortieDeCableType)
 
   // Simplistic total price calculation for demonstration
-  const totalPrice = prices.lameSelection + prices.dimensions + prices.installationType + prices.selectedColors + prices.ManoeuvreType;
+  const totalPrice = prices.lameSelection + prices.dimensions + prices.installationType + prices.selectedColors + prices.ManoeuvreType
 
   return (
     <div className="info-display">
@@ -38,7 +38,9 @@ const MultiStepInfoDisplay = () => {
           </tr>
           <tr>
             <th>Dimensions</th>
-            <td>Largeur: {dimensions.Largeur} mm, Hauteur: {dimensions.Hauteur} mm</td>
+            <td>
+              Largeur: {dimensions.Largeur} mm, Hauteur: {dimensions.Hauteur} mm
+            </td>
             <td className="price">{prices.dimensions}€</td>
           </tr>
           <tr>
@@ -48,7 +50,11 @@ const MultiStepInfoDisplay = () => {
           </tr>
           <tr>
             <th>Couleurs</th>
-            <td>Coulisse: {selectedCoulisseColor},<br/>Tablier: {selectedTablierColor},<br/>Lame Finale: {selectedLameFinaleColor}</td>
+            <td>
+              Coulisse: {selectedCoulisseColor},<br />
+              Tablier: {selectedTablierColor},<br />
+              Lame Finale: {selectedLameFinaleColor}
+            </td>
             <td className="price">{prices.selectedColors}€</td>
           </tr>
           <tr>
@@ -60,7 +66,7 @@ const MultiStepInfoDisplay = () => {
             <tr>
               <th>Outil de commande</th>
               <td>{ManualType}</td>
-              <td className="price">Included</td>
+              <td className="price">150€</td>
             </tr>
           )}
           {ManoeuvreType === 'Motorisé' && (
@@ -68,31 +74,48 @@ const MultiStepInfoDisplay = () => {
               <tr>
                 <th>Type de motorisation</th>
                 <td>{MotoriseType}</td>
-                <td className="price">Included</td>
+                <td className="price">220€</td>
               </tr>
-              {MotoriseType !== 'Filaire' && (
+
+              {MotoriseType === 'Radio' && (
                 <tr>
                   <th>Télécomande</th>
                   <td>{TelecommandeType}</td>
-                  <td className="price">Included</td>
+                  <td className="price">50€</td>
                 </tr>
               )}
-              <tr>
-                <th>Interrepteur</th>
-                <td>{InterrupteurType}</td>
-                <td className="price">Included</td>
-              </tr>
-              <tr>
-                <th>Sortie de cable</th>
-                <td>{SortieDeCableType}</td>
-                <td className="price">Included</td>
-              </tr>
+              {MotoriseType === 'Filaire' && (
+                <>
+                  <tr>
+                    <th>Interrepteur</th>
+                    <td>{InterrupteurType}</td>
+                    <td className="price">20€</td>
+                  </tr>
+                  <tr>
+                    <th>Sortie de cable</th>
+                    <td>{SortieDeCableType}</td>
+                    <td className="price">25€</td>
+                  </tr>
+                </>
+              )}
             </>
           )}
         </tbody>
       </table>
+      <table style={{ marginTop: '10px', width: '205px', textAlign: 'right', marginLeft: 'auto', marginRight: '0' }}>
+        <tbody>
+        <tr>
+            <th>Total HT</th>
+            <td className="price">{totalPrice}€</td>
+          </tr>
+          <tr>
+            <th>Total TTC</th>
+            <td className="price">{totalPrice}€</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  );
-};
+  )
+}
 
-export default MultiStepInfoDisplay;
+export default MultiStepInfoDisplay
--- src/components/steps/manoeuvre.js
+++ src/components/steps/manoeuvre.js
@@ -1,86 +1,80 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  setManoeuvreType, setManualType, setMotoriseType,
-  setTelecommandeType, setCommandeType, setInterrupteurType,
-  setSortieDeCableType
-} from '../../features/voletSlice';
-import ManualSelector from './Manoeuvre/ManualSelector';
-import MotoriseSelector from './Manoeuvre/MotoriseSelector';
-import TelecommandeSelector from './Manoeuvre/TelecommandeSelector';
-import CommandeSelector from './Manoeuvre/CommandeSelector';
-import InterrupteurSelector from './Manoeuvre/InterrupteurSelector';
-import SortieDeCableSelector from './Manoeuvre/SortieDeCableSelector';
-import OptionSelector from './Manoeuvre/OptionSelector';
-import './manoeuvre.css';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setManoeuvreType, setManualType, setMotoriseType, setTelecommandeType, setCommandeType, setInterrupteurType, setSortieDeCableType } from '../../features/voletSlice'
+import ManualSelector from './Manoeuvre/ManualSelector'
+import MotoriseSelector from './Manoeuvre/MotoriseSelector'
+import TelecommandeSelector from './Manoeuvre/TelecommandeSelector'
+import CommandeSelector from './Manoeuvre/CommandeSelector'
+import InterrupteurSelector from './Manoeuvre/InterrupteurSelector'
+import SortieDeCableSelector from './Manoeuvre/SortieDeCableSelector'
+import OptionSelector from './Manoeuvre/OptionSelector'
+import './manoeuvre.css'
 
-function Manoeuvre() {
-  const dispatch = useDispatch();
-  const {
-    ManoeuvreType, ManualType, MotoriseType, TelecommandeType,
-    CommandeType, InterrupteurType, SortieDeCableType
-  } = useSelector((state) => state.volet);
+function Manoeuvre({ enableNextButton }) {
+  const dispatch = useDispatch()
+  const { ManoeuvreType, ManualType, MotoriseType, TelecommandeType, CommandeType, InterrupteurType, SortieDeCableType } = useSelector((state) => state.volet)
+
+  useEffect(() => {
+    let isEnabled = false
+
+    // Check if the base ManoeuvreType has been selected
+    if (ManoeuvreType === 'Manuel') {
+      isEnabled = ManualType !== '' // Ensure a manual type has been selected
+    } else if (ManoeuvreType === 'Motorisé') {
+      // Ensure a motorization type has been selected
+      if (MotoriseType) {
+        if (MotoriseType === 'Radio') {
+          isEnabled = TelecommandeType !== ''
+        } else if (MotoriseType === 'Filaire') {
+          isEnabled = InterrupteurType !== '' && SortieDeCableType !== ''
+        }
+      }
+    }
+
+    // Call the enableNextButton with the resolved isEnabled status
+    enableNextButton(isEnabled)
+  }, [ManoeuvreType, ManualType, MotoriseType, TelecommandeType, InterrupteurType, SortieDeCableType])
 
   const handleChange = (setType) => (option) => {
-    dispatch(setType(option.label));
+    dispatch(setType(option.label))
   }
 
   const choices = [
     { label: 'Manuel', description: 'Fonctionne par une commande manuelle.', image: '../../assets/manuel.png' },
     { label: 'Motorisé', description: 'Actionné électriquement', image: '../../assets/motorise.png' }
-  ];
+  ]
 
   return (
     <div className="ma-container">
-      <OptionSelector
-        options={choices}
-        selectedOption={ManoeuvreType}
-        handleChange={handleChange(setManoeuvreType)}
-        type="choice"
-      />
+      <OptionSelector options={choices} selectedOption={ManoeuvreType} handleChange={handleChange(setManoeuvreType)} type="choice" />
       {ManoeuvreType === 'Manuel' && (
         <>
-        <h2 className="text">Outil De Commande</h2>
-        <ManualSelector
-          selectedOption={ManualType}
-          handleChange={handleChange(setManualType)}
-        />
-      </>
+          <h2 className="text">Outil De Commande</h2>
+          <ManualSelector selectedOption={ManualType} handleChange={handleChange(setManualType)} />
+        </>
       )}
       {ManoeuvreType === 'Motorisé' && (
         <>
           <h2 className="text">Type de Motorisation (marque : Becker)</h2>
-          <MotoriseSelector
-            selectedOption={MotoriseType}
-            handleChange={handleChange(setMotoriseType)}
-          />
+          <MotoriseSelector selectedOption={MotoriseType} handleChange={handleChange(setMotoriseType)} />
           {MotoriseType === 'Radio' && (
             <>
-              <h2 className="text">Télécommande :</h2>
-              <TelecommandeSelector
-                selectedOption={TelecommandeType}
-                handleChange={handleChange(setTelecommandeType)}
-              />
+              <h2 className="text">Télécommande</h2>
+              <TelecommandeSelector selectedOption={TelecommandeType} handleChange={handleChange(setTelecommandeType)} />
             </>
           )}
           {MotoriseType === 'Filaire' && (
             <>
               <h2 className="text">Interrupteur</h2>
-              <InterrupteurSelector
-                selectedOption={InterrupteurType}
-                handleChange={handleChange(setInterrupteurType)}
-              />
+              <InterrupteurSelector selectedOption={InterrupteurType} handleChange={handleChange(setInterrupteurType)} />
+              <h2 className="text">Sortie de câble</h2>
+              <SortieDeCableSelector selectedOption={SortieDeCableType} handleChange={handleChange(setSortieDeCableType)} />
             </>
           )}
-            <h2 className="text">Sortie de câble</h2>
-      <SortieDeCableSelector
-        selectedOption={SortieDeCableType}
-        handleChange={handleChange(setSortieDeCableType)}
-      />
         </>
       )}
     </div>
-  );
+  )
 }
 
-export default Manoeuvre;
+export default Manoeuvre
